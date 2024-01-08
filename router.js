a97713d08545a4f4e6e34fba4e31262d583c41e9
dekaratas <-C>
2024-01-08 08:46:27 +0100

    Feat: Add route to change user pw

--- router.js
+++ router.js
@@ -3,10 +3,12 @@ const userController = require("./controllers/user_controller");
 const bookController = require("./controllers/book_controller");
 
 router.post("/user", userController.createUser);
+router.patch("/user/:id", userController.patchUser);
+router.delete("/user/:id", userController.deleteUser);
+
 router.post("/books", bookController.createBook);
 router.get("/books", bookController.listBooks);
 router.patch("/books/:id", bookController.patchBooks);
-router.delete("/user/:id", userController.deleteUser);
 router.delete("/books/:id", bookController.deleteBook);
 
 module.exports = router;
